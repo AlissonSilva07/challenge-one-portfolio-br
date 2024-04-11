08f5dc69a3c7f4f5a7bc3e0c2063a566f4c5efcc
Alisson Rego <alissondroid07@gmail.com>
2024-04-11 11:37:58 -0300

    criado o componente de academics e projetos

--- validate.js
+++ validate.js
@@ -1,1 +1,58 @@
+const projectsSpace = document.getElementById('projects_area');
+
 //Seu JavaScript de validação aqui
+
+
+// Pegar os projetos do github
+async function getGitHubRepos() {
+    try {
+        const response = await fetch('https://api.github.com/users/alissonsilva07/repos');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function renderizarDados() {
+    const projectsArea = document.querySelector('.projects__items');
+    const data = await getGitHubRepos();
+
+    if (!data) {
+        return;
+    }
+
+    const repos = []
+
+    repos.push(data[4])
+    repos.push(data[7])
+    repos.push(data[15])
+    repos.push(data[16])
+    repos.push(data[22])
+    repos.push(data[23])
+
+    repos.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('projects__item');
+
+        const title = document.createElement('h2');
+        title.textContent = item.name;
+        title.classList.add('projects__container__title');
+
+        const description = document.createElement('p');
+        description.textContent = item.description;
+        description.classList.add('projects__container__description');
+
+        const language = document.createElement('p');
+        language.textContent = item.language;
+        language.classList.add('projects__container__language');
+
+        card.appendChild(title);
+        card.appendChild(description);
+        card.appendChild(language);
+        projectsArea.appendChild(card);
+    });
+}
+
+renderizarDados()
+
